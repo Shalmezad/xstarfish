3e9996d9fd37be4467396274479a4b8dbb381f35
Adrian Bridgett <bridgett@debian.org>
2002-04-11 20:58:11 +0100

    Import Debian changes 1.1-5
    xstarfish (1.1-5) unstable; urgency=low
    
      * lintian fix (remove emacs settings from changelog)
    
    xstarfish (1.1-4) unstable; urgency=low
    
      * add "Conflict: starfish" too (closes: #138816) md5sums were removed a
        while ago.
    
    xstarfish (1.1-3) unstable; urgency=low
    
      * remove "and" from build depends line (closes: #85063)
    
    xstarfish (1.1-2) unstable; urgency=low
    
      * add build depends (closes: #84665)
    
    xstarfish (1.1-1) unstable; urgency=low
    
      * new upstream
      * change name to xstarfish since that's what the binary is now called
      * fix menu entry (closes: #79644)
    
    starfish (1.0.b3-2) unstable; urgency=low
    
      * Doh! don't compile on an XFree86 v4 box (yet)
    
    starfish (1.0.b3-1) unstable; urgency=low
    
      * new upstream incorporating most fixes but not the enhancements (yet)
    
    starfish (1.0.b1-8) unstable; urgency=low
    
      * the Tom Lear <tom@trap.mtview.ca.us> edition:
        - fixes misinterpretation of hours,days,weeks... (closes: #68733)
        - adds interpolated zoom option (closes #68753)
        - "-p" option will create $HOME/.xstarfish$DISPLAY.pid file when in
          daemon mode (closes #68765)
        - GPG key seems to be installed now - first upload
    
    starfish (1.0.b1-7) unstable; urgency=low
    
      * diff was slightly off - it didn't fail for me, but hopefully this
        will fix it (closes: #67065)
    
    starfish (1.0.b1-6) unstable; urgency=low
    
      * fix several bugs for 24bit mode. ripped out all the junk
        and cleaned up what was left - should work everywhere
        (not sure if it's endian-clean) (closes: #64507)
      * added -r option to seed the random number generator for debugging
      * replaces sqrt(powf(x,2)+powf(y,2)) with hypotf(x,y) since it's stacks
        faster (17% in at least one case)
      * TODO: the argument handling needs rewriting using Getopt
    
    starfish (1.0.b1-5) unstable; urgency=low
    
      * Add manpage (closes: #64423)
    
    starfish (1.0.b1-4) unstable; urgency=low
    
      * X windows -> X in package description (thanks to Branden Robinson)
    
    starfish (1.0.b1-3) unstable; urgency=low
    
      * fix thinko on "-s" fix
      * fix final (I hope) memory leak
      * renamed program (but not yet package) to xstarfish to avoid clash with
        xscreensaver (thanks to  Owen Cameron <nemo@nethouse.goldweb.com.au>)
    
    starfish (1.0.b1-2) unstable; urgency=low
    
      * fixed memory leak
      * doesn't segfault if passsed "-s" last
    
    starfish (1.0.b1-1) unstable; urgency=low
    
      * initial release.

--- unix/setdesktop.c
+++ unix/setdesktop.c
@@ -43,12 +43,14 @@ int compose(int i, int shift)
   return (shift<0) ? (i>>(-shift)) : (i<<shift); 
 }
 
-void fillimage(StarfishRef tex)
+void fillimage(StarfishRef tex, int xzoom, int yzoom)
 {
   int x,y;
+  int dx,dy,i;
   unsigned long value;
   int redshift,greenshift,blueshift;
-  pixel pixel;
+  pixel pixel, oldpix;
+  float xstep,ystep,pixr,pixg,pixb,dr,dg,db;
      
   x=image->red_mask; redshift=-8;
   while(x) { x/=2; redshift++; }
@@ -57,16 +59,70 @@ void fillimage(StarfishRef tex)
   x=image->blue_mask; blueshift=-8;
   while(x) { x/=2; blueshift++; }
      
+  dy=0;
+  xstep=1.0/xzoom;
+  ystep=1.0/yzoom;
   for (y=0; y<height; y++)
   {
+    dx=0;
     for (x=0; x<width; x++)
     {
       GetStarfishPixel(x, y, tex, &pixel);
       value  = compose(pixel.red,redshift) & image->red_mask;
       value += compose(pixel.green,greenshift) & image->green_mask;
       value += compose(pixel.blue,blueshift) & image->blue_mask;
-      XPutPixel(image,x,y,value);
+      // we want the end of the zoomed area to get the pixel...
+      XPutPixel(image,dx+xzoom-1,dy,value);
+      // so that we can generate the interpolated pixels to it's left.
+      if(xzoom>1) {
+        if(x==0) GetStarfishPixel(width-1, y, tex, &oldpix);
+        dr=(pixel.red-oldpix.red)*xstep;
+        dg=(pixel.green-oldpix.green)*xstep;
+        db=(pixel.blue-oldpix.blue)*xstep;
+        pixr=pixel.red;
+        pixg=pixel.green;
+        pixb=pixel.blue;
+        for(i=xzoom-2; i>=0; i--) {
+          pixr-=dr;
+          pixg-=dg;
+          pixb-=db;
+          value  = compose(pixr,redshift) & image->red_mask;
+          value += compose(pixg,greenshift) & image->green_mask;
+          value += compose(pixb,blueshift) & image->blue_mask;
+          XPutPixel(image,dx+i,dy,value);
+        }
+        memcpy(&oldpix, &pixel, sizeof(pixel));
+      }
+      dx+=xzoom;
     }
+    if(y>0) {
+      do {
+        for(dx=0; dx<(width*xzoom); dx++) {
+          if(y==height) value=XGetPixel(image,dx,0);
+          else value=XGetPixel(image,dx,dy);
+          pixr=(value&image->red_mask)>>redshift;
+          pixg=(value&image->green_mask)>>greenshift;
+          pixb=(value&image->blue_mask)>>blueshift;
+          value=XGetPixel(image,dx,dy-yzoom);
+          dr=(value&image->red_mask)>>redshift;
+          dg=(value&image->green_mask)>>greenshift;
+          db=(value&image->blue_mask)>>blueshift;
+          dr=(pixr-dr)*ystep;
+          dg=(pixg-dg)*ystep;
+          db=(pixb-db)*ystep;
+          for(i=1; i<=yzoom; i++) {
+            pixr-=dr;
+            pixg-=dg;
+            pixb-=db;
+            value  = compose(pixr,redshift) & image->red_mask;
+            value += compose(pixg,greenshift) & image->green_mask;
+            value += compose(pixb,blueshift) & image->blue_mask;
+            XPutPixel(image,dx,dy-i,value);
+          }
+        }
+      } while(y==height-1 && y++ && (dy+=yzoom));
+    }
+    dy+=yzoom;
   }
 }
 
@@ -83,7 +139,7 @@ void XSetWindowBackgroundImage(Display* display, Drawable window, XImage* image)
   }
 }
 
-void mainloop(StarfishRef tex)
+void mainloop(StarfishRef tex, int xzoom, int yzoom)
 {
   char *buf;
   int bpl;
@@ -100,17 +156,17 @@ void mainloop(StarfishRef tex)
       {
         int pad, pad_bytes;
         bpp = pmf[i].bits_per_pixel;
-	bpl = width * bpp / 8;
+	bpl = width * xzoom * bpp / 8;
 	pad = pmf[i].scanline_pad;
 	pad_bytes = pad / 8;
 	/* make bpl a whole multiple of pad/8 */
 	bpl = (bpl + pad_bytes - 1) & ~(pad_bytes - 1);
-        buf=malloc(height*bpl);
+        buf=malloc(height*bpl*yzoom);
         image=XCreateImage(display,DefaultVisual(display,screen), depth,
-       	                   ZPixmap,0,buf,width,height,pad,bpl);
+       	                   ZPixmap,0,buf,width*xzoom,height*yzoom,pad,bpl);
         if(!image)
         {
-          puts("starfish: XCreateImage failed");
+          puts("xstarfish: XCreateImage failed");
           return;
         }
 	break;
@@ -121,12 +177,12 @@ void mainloop(StarfishRef tex)
 
   if (! XInitImage(image))
     return;
-  fillimage(tex);
+  fillimage(tex, xzoom, yzoom);
   XSetWindowBackgroundImage(display, rootwin, image);
   XDestroyImage(image);
 }
 
-void SetXDesktop(StarfishRef tex, const char* displayname)
+void SetXDesktop(StarfishRef tex, const char* displayname, int xzoom, int yzoom)
 {
   if (! (display = XOpenDisplay(displayname)))
   {
@@ -139,7 +195,7 @@ void SetXDesktop(StarfishRef tex, const char* displayname)
   gc=DefaultGC(display,screen);
   width = StarfishWidth(tex);
   height = StarfishHeight(tex);
-  mainloop(tex);
+  mainloop(tex, xzoom, yzoom);
   XCloseDisplay(display);
   return;
 }
